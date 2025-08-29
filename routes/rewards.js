bc75fe8c27765272839fe76584c853dcd669acdb
Edward-1100 <edward.w.hernandez1100@gmail.com>
2025-08-28 20:13:47 -0600

    Fixed Rewards.js?

--- routes/rewards.js
+++ routes/rewards.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { isLoggedIn } = require('../middleware/auth');
 
-router.get('/', isLoggedIn, (req, res) => {
-  res.render('rewards');
-});
+let authMiddleware = null;
+try {
+  const auth = require('../middleware/auth');
+  authMiddleware = auth.isLoggedIn || auth.ensureLoggedIn || auth.isLoggedIn || auth.ensure_logged_in || null;
+} catch (err) {
+  authMiddleware = null;
+}
+
+const renderRewards = (req, res) => {
+  res.render('rewards', { title: 'Rewards' });
+};
+
+if (authMiddleware && typeof authMiddleware === 'function') {
+  router.get('/', authMiddleware, renderRewards);
+} else {
+  router.get('/', renderRewards);
+}
 
 module.exports = router;
